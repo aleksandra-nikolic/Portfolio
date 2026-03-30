3393ba074aacc6bfdaae9906f8f0805209be1a2d
Alek <aleksandranikolic012@gmail.com>
2026-03-30 13:59:50 +0200

    build home/about/project/footer components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { Home } from './components/home/home';
+import { About } from './components/about/about';
+import { Projects } from './components/projects/projects';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Home, About, Projects, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('portfolio2');
